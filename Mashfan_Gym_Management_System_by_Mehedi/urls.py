73438c6fab432661a2e6dabe9a3b9d34118e8c60
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-10-25 12:36:27 +0600

    Saving to GitHub

--- Mashfan_Gym_Management_System_by_Mehedi/urls.py
+++ Mashfan_Gym_Management_System_by_Mehedi/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gym.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', About, name='about'),
+    path('contact', Contact, name='contact'),
 ]
--- gym/views.py
+++ gym/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def About(request):
+    return render(request, 'about.html')
+
+def Contact(request):
+    return render(request, 'contact.html')
