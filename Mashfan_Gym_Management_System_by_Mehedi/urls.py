33348e16185e72100feb00767c9ac1ce5a0acb48
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-10-28 23:57:21 +0600

    Saving to GitHub

--- Mashfan_Gym_Management_System_by_Mehedi/urls.py
+++ Mashfan_Gym_Management_System_by_Mehedi/urls.py
@@ -24,7 +24,13 @@ urlpatterns = [
     path('', Index, name='home'),
     path('admin_login', Login, name='login'),
     path('logout', Logout_admin, name='logout'),
+
     path('add_enquiry', Add_Enquiry, name='add_enquiry'),
     path('view_enquiry', View_Enquiry, name='view_enquiry'),
+    path('delete_enquiry(?P<int:pid>)', Delete_Enquiry, name='delete_enquiry'),
+
+    path('add_equipment', Add_Equipment, name='add_quipment'),
+    path('view_equipment', View_Equipment, name='view_equipment'),
+    path('delete_equipment(?P<int:pid>)', Delete_Equipment, name='delete_equipment'),
 
 ]
